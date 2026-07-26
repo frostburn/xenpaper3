d25c35bce28bc22f477c17127331ee8c951db775
Lumi Pakkanen <lumi.pakkanen@gmail.com>
2026-07-26 18:15:14 +0300

    Add SW Patch parser type declarations and tighten parser test types

--- sw-patch/__test__/parser.spec.ts
+++ sw-patch/__test__/parser.spec.ts
@@ -1,10 +1,10 @@
 import { describe, it, expect } from 'vitest'
-import {parse} from '../parser.generated.js'
+import { parse, type Node } from '../parser.generated.js'
 
-function stripLocation(node) {
-  const result = {...node}
+function stripLocation<T extends Node>(node: T): Omit<T, 'location'> {
+  const result: Partial<T> = { ...node }
   delete result.location
-  return result
+  return result as Omit<T, 'location'>
 }
 
 describe('SW Patch parser', () => {
@@ -21,29 +21,45 @@ if true:
     expect(ast.body).toHaveLength(4)
 
     // Comments are statements for syntax-highlighting purposes
-    expect(ast.body[0].type).toBe('CommentStatement')
-    expect(ast.body[0].value).toBe(' First line')
-    expect(ast.body[0].location).toEqual({
+    const [comment, topLevelExpression, conditional, finalExpression] = ast.body
+
+    expect(comment).toMatchObject({
+      type: 'CommentStatement',
+      value: ' First line'
+    })
+    expect(comment?.location).toEqual({
       source: undefined,
       start: { offset: 0, line: 1, column: 1 },
       end: { offset: 12, line: 1, column: 13 }
     })
 
-    expect(ast.body[1].type).toBe('ExpressionStatement')
-    expect(stripLocation(ast.body[1].expression)).toEqual({
+    expect(topLevelExpression?.type).toBe('ExpressionStatement')
+    if (topLevelExpression?.type !== 'ExpressionStatement') {
+      throw new Error('Expected expression')
+    }
+    expect(stripLocation(topLevelExpression.expression)).toEqual({
       type: 'StringLiteral',
       value: 'top level string'
     })
 
-    expect(ast.body[2].body).toHaveLength(1)
-    expect(ast.body[2].body[0].type).toBe('ExpressionStatement')
-    expect(stripLocation(ast.body[2].body[0].expression)).toEqual({
+    expect(conditional?.type).toBe('IfStatement')
+    if (conditional?.type !== 'IfStatement') throw new Error('Expected if statement')
+    expect(conditional.body).toHaveLength(1)
+    const nestedExpression = conditional.body[0]
+    expect(nestedExpression?.type).toBe('ExpressionStatement')
+    if (nestedExpression?.type !== 'ExpressionStatement') {
+      throw new Error('Expected expression')
+    }
+    expect(stripLocation(nestedExpression.expression)).toEqual({
       type: 'StringLiteral',
       value: 'second level string'
     })
 
-    expect(ast.body[3].type).toBe('ExpressionStatement')
-    expect(stripLocation(ast.body[3].expression)).toEqual({
+    expect(finalExpression?.type).toBe('ExpressionStatement')
+    if (finalExpression?.type !== 'ExpressionStatement') {
+      throw new Error('Expected expression')
+    }
+    expect(stripLocation(finalExpression.expression)).toEqual({
       type: 'StringLiteral',
       value: 'back to top level'
     })
--- sw-patch/parser.generated.d.ts
+++ sw-patch/parser.generated.d.ts
@@ -1,1 +1,298 @@
 import type { LocationRange } from 'peggy'
+
+export interface Node {
+  type: string
+  location: LocationRange
+}
+
+export interface Program extends Node {
+  type: 'Program'
+  body: Statement[]
+}
+
+export type Statement =
+  | TriggerHandler
+  | WithStatement
+  | IfStatement
+  | ElifStatement
+  | ElseStatement
+  | TypeAlias
+  | ConstDeclaration
+  | ParamDeclaration
+  | TypedBinding
+  | ScheduledStatement
+  | ConnectionStatement
+  | AssignmentStatement
+  | DocStringStatement
+  | CommentStatement
+  | ExpressionStatement
+
+export interface TriggerHandler extends Node {
+  type: 'TriggerHandler'
+  trigger: Expression
+  body: Statement[]
+}
+
+export interface WithStatement extends Node {
+  type: 'WithStatement'
+  resource: Expression
+  body: Statement[]
+}
+
+export interface IfStatement extends Node {
+  type: 'IfStatement'
+  test: Expression
+  body: Statement[]
+}
+
+export interface ElifStatement extends Node {
+  type: 'ElifStatement'
+  test: Expression
+  body: Statement[]
+}
+
+export interface ElseStatement extends Node {
+  type: 'ElseStatement'
+  body: Statement[]
+}
+
+export interface TypeAlias extends Node {
+  type: 'TypeAlias'
+  name: string
+  value: TypeExpression
+}
+
+export interface ConstDeclaration extends Node {
+  type: 'ConstDeclaration'
+  name: string
+  annotation: TypeExpression | null
+  value: Expression
+}
+
+export interface ParamDeclaration extends Node {
+  type: 'ParamDeclaration'
+  name: string
+  annotation: TypeExpression
+  value: Expression
+}
+
+export interface TypedBinding extends Node {
+  type: 'TypedBinding'
+  name: string
+  annotation: TypeExpression
+  value: Expression
+}
+
+export interface ScheduledStatement extends Node {
+  type: 'ScheduledStatement'
+  at: Expression
+  automation: Automation | null
+  statement: AssignmentStatement | ConnectionStatement | ExpressionStatement
+}
+
+export type Automation = LinearAutomation | ExponentialAutomation | TargetAutomation
+
+export interface LinearAutomation extends Node {
+  type: 'LinearAutomation'
+}
+
+export interface ExponentialAutomation extends Node {
+  type: 'ExponentialAutomation'
+}
+
+export interface TargetAutomation extends Node {
+  type: 'TargetAutomation'
+  timeConstant: Expression
+}
+
+export interface ConnectionStatement extends Node {
+  type: 'ConnectionStatement'
+  first: Expression
+  links: ConnectionLink[]
+}
+
+export interface ConnectionLink {
+  operator: 'connect' | 'disconnect'
+  target: Expression
+}
+
+export interface AssignmentStatement extends Node {
+  type: 'AssignmentStatement'
+  target: Identifier | MemberExpression
+  value: Expression
+}
+
+export interface DocStringStatement extends Node {
+  type: 'DocStringStatement'
+  value: string
+}
+
+export interface CommentStatement extends Node {
+  type: 'CommentStatement'
+  value: string
+}
+
+export interface ExpressionStatement extends Node {
+  type: 'ExpressionStatement'
+  expression: Expression
+}
+
+export type TypeExpression =
+  | UnionType
+  | GenericType
+  | ObjectType
+  | StringLiteralType
+  | TypeName
+
+export interface UnionType extends Node {
+  type: 'UnionType'
+  types: TypeExpression[]
+}
+
+export interface GenericType extends Node {
+  type: 'GenericType'
+  callee: TypeName
+  argument: TypeExpression
+}
+
+export interface ObjectType extends Node {
+  type: 'ObjectType'
+  fields: TypeField[]
+}
+
+export interface TypeField extends Node {
+  type: 'TypeField'
+  name: string
+  value: TypeExpression
+}
+
+export interface StringLiteralType extends Node {
+  type: 'StringLiteralType'
+  value: string
+}
+
+export interface TypeName extends Node {
+  type: 'TypeName'
+  name: string
+}
+
+export type Expression =
+  | BinaryExpression
+  | UnaryExpression
+  | MemberExpression
+  | CallExpression
+  | UnitLiteral
+  | NumberLiteral
+  | BooleanLiteral
+  | NullLiteral
+  | StringLiteral
+  | ObjectLiteral
+  | ListLiteral
+  | Identifier
+
+export interface BinaryExpression extends Node {
+  type: 'BinaryExpression'
+  operator:
+    | 'or'
+    | 'and'
+    | '<='
+    | '>='
+    | '=='
+    | '!='
+    | '<'
+    | '>'
+    | '+'
+    | '-'
+    | '*'
+    | '/'
+    | '%'
+  left: Expression
+  right: Expression
+}
+
+export interface UnaryExpression extends Node {
+  type: 'UnaryExpression'
+  operator: '+' | '-' | '!' | 'not'
+  argument: Expression
+}
+
+export interface MemberExpression extends Node {
+  type: 'MemberExpression'
+  object: Expression
+  property: string
+}
+
+export interface CallExpression extends Node {
+  type: 'CallExpression'
+  callee: Expression
+  arguments: Argument[]
+}
+
+export type Argument = NamedArgument | PositionalArgument
+
+export interface NamedArgument extends Node {
+  type: 'NamedArgument'
+  name: string
+  value: Expression
+}
+
+export interface PositionalArgument extends Node {
+  type: 'PositionalArgument'
+  value: Expression
+}
+
+export interface ObjectLiteral extends Node {
+  type: 'ObjectLiteral'
+  entries: ObjectEntry[]
+}
+
+export interface ObjectEntry extends Node {
+  type: 'ObjectEntry'
+  key: string
+  value: Expression
+}
+
+export interface ListLiteral extends Node {
+  type: 'ListLiteral'
+  elements: Expression[]
+}
+
+export interface UnitLiteral extends Node {
+  type: 'UnitLiteral'
+  value: string
+  unit: string
+  spaced: boolean
+}
+
+export interface NumberLiteral extends Node {
+  type: 'NumberLiteral'
+  value: string
+}
+
+export interface BooleanLiteral extends Node {
+  type: 'BooleanLiteral'
+  value: boolean
+}
+
+export interface NullLiteral extends Node {
+  type: 'NullLiteral'
+  value: null
+}
+
+export interface StringLiteral extends Node {
+  type: 'StringLiteral'
+  value: string
+}
+
+export interface Identifier extends Node {
+  type: 'Identifier'
+  name: string
+}
+
+export interface ParseOptions {
+  grammarSource?: string
+  startRule?: 'Start'
+  [key: string]: unknown
+}
+
+export function parse(input: string, options?: ParseOptions): Program
