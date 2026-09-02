d3fe6460539f4b6683ce8d4e6b68005bcb42e6c1
Lumi Pakkanen <lumi.pakkanen@gmail.com>
2026-09-02 16:41:08 +0300

    Chunk production build by pseudo-package

--- vite.config.ts
+++ vite.config.ts
@@ -15,4 +15,29 @@ export default defineConfig({
       '@': fileURLToPath(new URL('./src', import.meta.url)),
     },
   },
+  build: {
+    rolldownOptions: {
+      output: {
+        codeSplitting: {
+          groups: [
+            {
+              name: 'xenpaper-lang',
+              test: /[\\/]xenpaper-lang[\\/]/,
+              includeDependenciesRecursively: false,
+            },
+            {
+              name: 'sw-patch',
+              test: /[\\/]sw-patch[\\/]/,
+              includeDependenciesRecursively: false,
+            },
+            {
+              name: 'sw-seq',
+              test: /[\\/]sw-seq[\\/]/,
+              includeDependenciesRecursively: false,
+            },
+          ],
+        },
+      },
+    },
+  },
 })
